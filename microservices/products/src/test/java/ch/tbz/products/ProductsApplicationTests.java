6ffdf704edcdffd2423201523126ed1982fcf2e0
Haradini Rilind <rilind.haradini@gmail.com>
2026-01-04 18:37:39 +0100

    Initialize Spring Boot project with Gradle setup and basic application structure

---
+++ microservices/products/src/test/java/ch/tbz/products/ProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.tbz.products;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
